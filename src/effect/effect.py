5ae76ccada631879fde7cf03b5c8511adfa5e726
xingchentye <xingchenawa@qq.com>
2024-03-03 16:58:12 +0800

    “update”

---
+++ src/effect/effect.py
@@ -0,0 +1,4 @@
+effect: dict = {
+    "effect.badOmen": "凶兆",
+    "effect.villageHero": "村庄英雄"
+}
---
+++ src/gameMode/gameMode.py
@@ -0,0 +1,8 @@
+gameMode: dict ={
+    "gameMode.adventure": "冒险模式",
+    "gameMode.changed": "您的游戏模式已更新为 {}",
+    "gameMode.creative": "创造模式",
+    "gameMode.hardcore": "极限模式！",
+    "gameMode.spectator": "旁观模式",
+    "gameMode.survival": "生存模式"
+}
